af83b16462859285e3e7188d616c900aa8c0c732
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-06-23 12:31:28 -0500

    ad list preliminar

---
+++ frontend/src/TopBar.jsx
@@ -0,0 +1,8 @@
+export default function TopBar() {
+    return <div className="top-bar">
+        <button>Inicio</button>
+        <button>Anuncios</button>
+        <button>Inventario</button>
+        <button>Pedidos</button>
+    </div>
+}
