ef75eaf792e34d0c9eed9eec61dd981f795cd60d
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-06-23 22:12:07 -0500

    fix image and backend for products
    Now the reference image is exactly contained in its container.
    
    Added entry point for getting product list

--- backend/app/database/json_db.py
+++ backend/app/database/json_db.py
@@ -1,1 +1,14 @@
+# Keeps fast access to variables and saves to db.json
+import json
 
+db = {}
+filepath="backend/app/database/db.json"
+
+def read_db():
+    return db
+
+def save_to_db(data):
+    global db
+    db = data
+    with open(filepath, "w") as f:
+        json.dump(db, f, indent=4)
