02ab81b5988391be5ff67fca1bd30fcc3dd12387
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-06-26 22:14:23 -0500

    look improvements
    The tag selection and publish button now are similar to the mockups.

--- frontend/src/components/administrador-anuncios/AdvertCard.jsx
+++ frontend/src/components/administrador-anuncios/AdvertCard.jsx
@@ -5,6 +5,7 @@ export default function AdvertCard({ advert }) {
         <div className="advert-card">
             <h3 className="advert-title">{advert.name}</h3>
             <h4>${advert.price.toLocaleString('es-CO')}</h4>
+            <h4>{advert.quantity} disponibles</h4>
             <img
                 className="advert-image"
                 src={advert.photos[0] || square_placeholder}
