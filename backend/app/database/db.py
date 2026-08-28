ab6ed011e6ac3d28016b28f34e229889cc02196b
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-08-28 17:10:14 -0500

    use hosted database

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -2,18 +2,21 @@
 import json
 from sqlalchemy import create_engine,engine
 from sqlalchemy.orm import DeclarativeBase
+import os
 
 filepath="backend/app/database/db.json"
 
+if not os.getenv("DATABASE_PASSWORD"):
+    raise ValueError("DATABASE_PASSWORD environment variable is not set.")
+
 engine = create_engine(engine.url.URL.create(
-    'postgresql',
-    username='postgres',
-    password='6920',
-    host='localhost',
-    port=54668,
-    database='ados'
-)
-)
+    drivername="postgresql",
+    username="postgres",
+    password=os.getenv("DATABASE_PASSWORD"),
+    host="db.wjudkciapfrdlrlxgqmk.supabase.co",
+    port=5432,
+    database="postgres"
+))
 
 class Base(DeclarativeBase):
     pass
