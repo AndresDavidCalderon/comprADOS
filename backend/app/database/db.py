50a3d6fd8ee83a74ad147fcd4d524aedab168b39
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-08-29 20:30:44 -0500

    more env variables for hosting

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -9,13 +9,22 @@ filepath="backend/app/database/db.json"
 if not os.getenv("DATABASE_PASSWORD"):
     raise ValueError("DATABASE_PASSWORD environment variable is not set.")
 
+if not os.getenv("DATABASE_HOST"):
+    raise ValueError("DATABASE_HOST environment variable is not set.")
+
+if not os.getenv("DATABASE_PORT"):
+    raise ValueError("DATABASE_PORT environment variable is not set.")
+
+if not os.getenv("DATABASE_NAME"):
+    raise ValueError("DATABASE_NAME environment variable is not set.")
+
 engine = create_engine(engine.url.URL.create(
     drivername="postgresql",
     username="postgres",
     password=os.getenv("DATABASE_PASSWORD"),
-    host="db.wjudkciapfrdlrlxgqmk.supabase.co",
-    port=5432,
-    database="postgres"
+    host=os.getenv("DATABASE_HOST", "localhost"),
+    port=int(os.getenv("DATABASE_PORT", "54668")),
+    database=os.getenv("DATABASE_NAME", "postgres")
 ))
 
 class Base(DeclarativeBase):
