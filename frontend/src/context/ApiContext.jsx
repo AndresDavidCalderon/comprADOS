6f34c1e32fc5e0d6d03db034bdf3e049c34ee7cd
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-08-29 21:20:44 -0500

    debug api url

--- frontend/src/context/ApiContext.jsx
+++ frontend/src/context/ApiContext.jsx
@@ -1,12 +1,17 @@
 import {createContext, useContext} from 'react';
 import AuthContext from './AuthContext.jsx';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const ApiContext = createContext({
-    apiUrl: 'http://localhost:8000'
+    apiUrl: 'URL NOT PROVIDED'
 });
 
 export function ApiProvider({ children }) {
-    const apiUrl = import.meta.env.VITE_API_URL;
+    if (!apiUrl) {
+        throw new Error('VITE_API_URL environment variable is not set.');
+    }
+    console.log(apiUrl);
     return <ApiContext.Provider value={{ apiUrl }}>{children}</ApiContext.Provider>;
 }
 
