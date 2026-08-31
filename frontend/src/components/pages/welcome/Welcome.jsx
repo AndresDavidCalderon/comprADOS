9186317b12c309181fa422f72fbcd78385b7982d
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2026-08-31 10:34:20 -0500

    Squashed commit of the following:
    commit 0b2feee73f482a780136e5a8c3f13f6a93d1f622
    Author: Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
    Date:   Sun Aug 30 18:17:09 2026 -0500
    
        change video file
    
    commit 26fc3edd737cf88cd8a345700b6a1ecc7b4c646e
    Merge: ee3bd40 e7d957b
    Author: Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
    Date:   Sun Aug 30 17:56:50 2026 -0500
    
        Merge branch 'main' into v2.2
    
    commit ee3bd40b0837aaf370d7ba4945a91976ceb47114
    Author: Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
    Date:   Sun Aug 30 12:33:50 2026 -0500
    
        mover video

--- frontend/src/components/pages/welcome/Welcome.jsx
+++ frontend/src/components/pages/welcome/Welcome.jsx
@@ -9,7 +9,7 @@ export default function Welcome({addToCart}) {
         <>
             <div className="home-video-container">
                 <video autoPlay muted loop className="home-video">
-                    <source src="/video-intro-web.mp4" type="video/mp4" />
+                    <source src="/video-intro-web-optimized.mp4" type="video/mp4" />
                 </video>
                 <header className="home-header">
                     <h1>Bienvenidos a <span>ADOS Me Gusta</span></h1>
